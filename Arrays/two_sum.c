090b6bb40b46c2f4fb5cd350bc63ff88e75856ac
ChitraShree Gowda <nanjundachitra100@gmail.com>
2026-09-17 22:31:57 +0530

    two sum

--- Arrays/two_sum.c
+++ Arrays/two_sum.c
@@ -1,10 +1,5 @@
 #include<stdio.h>
-//You are given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
-//You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-//You can return the answer in any order.
-    int* twoSum(int* nums, int numsSize, int target, int* returnSize)
+int* twoSum(int* nums, int numsSize, int target, int* returnSize)
 {
     int* ans = malloc(2 * sizeof(int));
 
@@ -28,4 +23,5 @@
     return ans;
 }
     
+
 
